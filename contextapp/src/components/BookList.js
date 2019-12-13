8bb196eca0d01b6793f7dc29a8a545b5ea9d306d
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-12-13 08:09:25 -0500

    Multiple Contexts using Hooks

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
-class BookList extends Component {
-	static contextType = ThemeContext;
 
-	render() {
-		const { isLightTheme, light, dark } = this.context;
+// class BookList extends Component {
+// 	// only use with class components
+// 	static contextType = ThemeContext;
 
-		const theme = isLightTheme ? light : dark;
+// 	render() {
+// 		const { isLightTheme, light, dark } = this.context;
 
-		return (
-			<div className="book-list" style={{ background: theme.ui, color: theme.syntax }}>
-				<ul style={{ background: theme.ui }}>the way of king</ul>
-				<ul style={{ background: theme.ui }}>the name of the wind</ul>
-				<ul style={{ background: theme.ui }}>the final empire</ul>
-			</div>
-		);
-	}
-}
+// 		const theme = isLightTheme ? light : dark;
+
+// 		return (
+// 			<div className="book-list" style={{ background: theme.ui, color: theme.syntax }}>
+// 				<ul style={{ background: theme.ui }}>the way of king</ul>
+// 				<ul style={{ background: theme.ui }}>the name of the wind</ul>
+// 				<ul style={{ background: theme.ui }}>the final empire</ul>
+// 			</div>
+// 		);
+// 	}
+// }
+
+const BookList = () => {
+	const { isLightTheme, light, dark } = useContext(ThemeContext);
+
+	const theme = isLightTheme ? light : dark;
+
+	return (
+		<div className="book-list" style={{ background: theme.ui, color: theme.syntax }}>
+			<ul style={{ background: theme.ui }}>the way of king</ul>
+			<ul style={{ background: theme.ui }}>the name of the wind</ul>
+			<ul style={{ background: theme.ui }}>the final empire</ul>
+		</div>
+	);
+};
 
 export default BookList;
