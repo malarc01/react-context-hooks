c64827f4de0e61f98238d737f17db2cfd0ca55fb
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-12-15 12:41:26 -0500

    added bookReducer.js

--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,33 +1,23 @@
-import React, {createContext, useState} from 'react';
-import uuid from 'uuid/v1'
-
-
+import React, { createContext, useState, useReducer } from 'react';
+import uuid from 'uuid/v1';
 
 export const BookContext = createContext();
 
-const BookContextProvider = (props) =>{
-   const [books, setBooks] = useState([
-        {title:'The Shock Doctrine', author:"Naomi Campbell",id:1},
-        {title:'Freakonomics', author:"Levit Stephen",id:2},
+const BookContextProvider = (props) => {
+	const [ books, setBooks ] = useState([
+		{ title: 'The Shock Doctrine', author: 'Naomi Campbell', id: 1 },
+		{ title: 'Freakonomics', author: 'Levit Stephen', id: 2 }
+	]);
 
-    ])
+	const addBook = (title, author) => {
+		setBooks([ ...books, { title: title, author: author, id: uuid() } ]);
+	};
 
-    const addBook = (title, author) =>{
-        setBooks([...books,{title:title, author:author, id:uuid()}])
+	const removeBook = (id) => {
+		setBooks(books.filter((book) => book.id !== id));
+	};
 
-    }
+	return <BookContext.Provider value={{ books, addBook, removeBook }}>{props.children}</BookContext.Provider>;
+};
 
-    const removeBook = (id) =>{
-        setBooks(books.filter(book=>book.id !== id))
-
-    }
-    
-    return(
-        <BookContext.Provider value={{books,addBook,removeBook}}>
-        {props.children}
-        </BookContext.Provider>
-    )
-}
-
-
-export default BookContextProvider
+export default BookContextProvider;
